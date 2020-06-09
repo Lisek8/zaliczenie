975d5313cd4a3f54d2d09cc48cfb15ece8765d68
Lis-Marciniak.Jędrzej <215799@edu.p.lodz.pl>
2020-06-09 18:19:56 +0200

    [BEHAVIORAL TEST] Added test for number of times of checking if door is open if checking filter's capacity

--- testsReactor/src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
@@ -2,18 +2,19 @@ package edu.iis.mto.testreactor.dishwasher;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.when;
 
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import edu.iis.mto.testreactor.dishwasher.engine.Engine;
 import edu.iis.mto.testreactor.dishwasher.pump.WaterPump;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class DishWasherTest {
@@ -65,10 +66,10 @@ class DishWasherTest {
     @Test
     public void shouldReturnOpenDoorErrorOnOpenDoor() {
         programConfiguration = ProgramConfiguration.builder()
-                .withProgram(irreleventWashingProgram)
-                .withTabletsUsed(irreleventTabletsUsed)
-                .withFillLevel(irreleventFillLevel)
-                .build();
+                                                   .withProgram(irreleventWashingProgram)
+                                                   .withTabletsUsed(irreleventTabletsUsed)
+                                                   .withFillLevel(irreleventFillLevel)
+                                                   .build();
         when(door.closed()).thenReturn(false);
         RunResult result = dishWasher.start(programConfiguration);
         assertSame(result.getStatus(), Status.DOOR_OPEN);
@@ -77,13 +78,29 @@ class DishWasherTest {
     @Test
     public void shouldReturnErrorFilterErrorOnDirtyFilter() {
         programConfiguration = ProgramConfiguration.builder()
-                .withProgram(irreleventWashingProgram)
-                .withTabletsUsed(irreleventTabletsUsed)
-                .withFillLevel(irreleventFillLevel)
-                .build();
+                                                   .withProgram(irreleventWashingProgram)
+                                                   .withTabletsUsed(irreleventTabletsUsed)
+                                                   .withFillLevel(irreleventFillLevel)
+                                                   .build();
         when(door.closed()).thenReturn(true);
         when(dirtFilter.capacity()).thenReturn(DishWasher.MAXIMAL_FILTER_CAPACITY - 1d);
         RunResult result = dishWasher.start(programConfiguration);
         assertSame(result.getStatus(), Status.ERROR_FILTER);
     }
+
+    // BEHAVIORAL TESTS
+    @Test
+    public void shouldCheckIfDoorIsClosedAndIfFilterIsClean() {
+        programConfiguration = ProgramConfiguration.builder()
+                                                   .withProgram(irreleventWashingProgram)
+                                                   .withTabletsUsed(true)
+                                                   .withFillLevel(irreleventFillLevel)
+                                                   .build();
+        when(door.closed()).thenReturn(true);
+        dishWasher.start(programConfiguration);
+        InOrder order = inOrder(door, dirtFilter);
+        order.verify(door).closed();
+        order.verify(dirtFilter).capacity();
+    }
+
 }
